a11b8feaee5cd132d082c137f080f44df6a1b998
AtthalaricNero <142994524+AtthalaricNero@users.noreply.github.com>
2023-11-01 19:58:41 +0700

    menambah Aku suka nasi goreng

--- LatihanGit.java
+++ LatihanGit.java
@@ -6,5 +6,6 @@ package WRI;
 public class LatihanGit {
     public static void main(String[] args) {
         System.out.println("Hai");
+        System.out.println("Aku suka nasi goreng");
     }
 }
