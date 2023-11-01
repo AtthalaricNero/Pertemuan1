65c0c5349fa16aab004f4f4899d17b51d8e30825
AtthalaricNero <142994524+AtthalaricNero@users.noreply.github.com>
2023-11-01 20:27:39 +0700

    Tambah int bilangan

--- LatihanGit.java
+++ LatihanGit.java
@@ -5,7 +5,9 @@ package WRI;
  */
 public class LatihanGit {
     public static void main(String[] args) {
+        int bilangan = 30;
         System.out.println("Hai");
         System.out.println("Aku suka nasi goreng");
+        System.out.println(bilangan);
     }
 }
